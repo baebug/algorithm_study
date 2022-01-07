cbd0c76f798de2b0c36e5c102902abfb63f21d4e
baebug <ufr134@gmail.com>
2022-01-07 17:32:09 +0900

    prime_number

--- py/algorithm/baekjoon/step9/11653.py
+++ py/algorithm/baekjoon/step9/11653.py
@@ -4,40 +4,24 @@
 정수 N 이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
 
 # 시간초과 뜸
+# n /= div 한 이후 n 이 소수인지 확인해야 함
+# root(n) 보다 작은 소수로 안나눠지면 소수임
 '''
 import sys
 
 def input():
     return sys.stdin.readline().strip()
 
-
-def isPrime(n):
-    if n in prime_list: return True
-
-    if n == 1: return False
-    elif n == 2: return True
-
-    div = 2
-    while n > div:
-        if n % div == 0:
-            return False
-        else:
-            div += 1
-    prime_list.append(n)
-    return True
-
 def solve(n):
     div = 2
-    while n != 1:
+    while div * div <= n:
         if n % div == 0:
-            n = n // div
             print(div)
+            n //= div
         else:
-            while 1:
-                div += 1
-                if isPrime(div): break
+            div += 2 if div > 2 else 1
+    if n > 1:
+        print(n)
 
-prime_list = [2]
-N = int(input())
+N = int(input())    
 solve(N)
-

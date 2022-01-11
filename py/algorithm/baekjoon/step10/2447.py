c511f96a094fa1684efe0f454686abc48df5251d
baebug <ufr134@gmail.com>
2022-01-11 17:10:27 +0900

    recursive

---
+++ py/algorithm/baekjoon/step10/2447.py
@@ -0,0 +1,31 @@
+'''
+재귀 - 별 찍기 - 10
+
+구글 검색함
+'''
+import sys
+
+def input():
+    return sys.stdin.readline().strip()
+
+def star(n, arr):
+    matrix = []
+
+    if n == 3:
+        return arr
+    else:
+        for i in arr:
+            matrix.append(i * 3)
+        for i in arr:
+            matrix.append(i + " " * len(arr) + i)
+        for i in arr:
+            matrix.append(i * 3)
+        return star(n//3, matrix)
+
+first = ["***", "* *", "***"]
+n = int(input())        
+
+ans = star(n, first)
+
+for i in ans:
+    print(i)

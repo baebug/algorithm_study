4b3d4580fd10e56999ce3a7bd5b62117d66fd94c
baebug <ufr134@gmail.com>
2022-07-26 22:57:58 +0900

    월말 평가 문제로 stream 연습 and add init file

---
+++ java/eclipse/src/ssafy/algorithm/Average.java
@@ -0,0 +1,16 @@
+package ssafy.algorithm;
+
+import java.util.Arrays;
+
+public class Average {
+
+	public static void main(String[] args) {
+		int[] su = {45, 80, 68, 19, 34, 55, 27, 60, 27, 18};
+		
+		double avg = Arrays.stream(su).average().getAsDouble();
+		int max = Arrays.stream(su).max().getAsInt();
+		System.out.printf("%.1f %d", avg, max);
+
+	}
+
+}

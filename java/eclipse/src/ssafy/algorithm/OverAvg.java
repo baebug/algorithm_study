0bd045785cba74b30768a0b75e7d76f93d47a2c6
baebug <ufr134@gmail.com>
2022-07-26 22:57:58 +0900

    월말 평가 문제로 stream 연습 and add init file

---
+++ java/eclipse/src/ssafy/algorithm/OverAvg.java
@@ -0,0 +1,41 @@
+package ssafy.algorithm;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class OverAvg {
+
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("input.txt"));
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int[] arr = new int[n];
+			int sum = 0;
+			
+			for (int i = 0; i < n; i++) {
+				int tmp = Integer.parseInt(st.nextToken());
+				arr[i] = tmp;
+				sum += tmp;
+			}
+			
+			double avg = (double) sum / n;
+			
+			long cnt = Arrays.stream(arr).filter(x -> x > avg).count();
+			
+			System.out.printf("#%d %.3f%%%n", t, (double) cnt / n * 100);
+			
+			
+		}
+
+	}
+
+}

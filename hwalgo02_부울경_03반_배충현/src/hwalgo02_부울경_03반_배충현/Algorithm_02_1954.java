052eb47f050d9afd7213af308ad10f6b083e84e8
baebug <ufr134@gmail.com>
2022-08-03 01:08:17 +0900

    과제 및 스터디 문제 풀이

---
+++ hwalgo02_부울경_03반_배충현/src/hwalgo02_부울경_03반_배충현/Algorithm_02_1954.java
@@ -0,0 +1,62 @@
+package hwalgo02_부울경_03반_배충현;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Algorithm_02_1954 {
+	
+	static int T, n;
+	static int[][] arr;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T; t++) {
+			n = Integer.parseInt(br.readLine());
+			sb = new StringBuilder();
+			
+			arr = new int[n][n];
+			
+			// 테두리 채우기, st_num 넘겨주기, n-2 재귀
+			snail(n, 0, 0, 1);
+
+			
+			System.out.println("#" + t);
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					sb.append(arr[i][j] + " ");
+				}
+				sb.append("\n");
+			}
+			System.out.print(sb);
+		}
+
+	}
+	
+	static void snail(int n, int y, int x, int num) {
+		if (n == 0) {
+			return;
+		} else if (n == 1) {
+			arr[y][x] = num;
+			return;
+		}
+		
+		// 양 모서리에서 시작해서 토네이도
+		for (int i = 0; i < n; i++) {
+			arr[y][x+i] = 			num + i;
+			arr[y+i][x+n-1] = 		num + i + (n-1);
+			arr[y+n-1][x+n-1-i] = 	num + i + 2 * (n-1);
+			arr[y+n-1-i][x] = 		num + i + 3 * (n-1);
+		}
+		
+		arr[y][x] = num;
+		
+		snail(n-2, y+1, x+1, num + 4 * (n-1));
+		
+		
+	}
+
+}
--- java/eclipse/src/ssafy/algorithm/pg/PG_기능개발.java
+++ java/eclipse/src/ssafy/algorithm/pg/PG_기능개발.java
@@ -7,48 +7,54 @@ import java.util.List;
 public class PG_기능개발 {
 
 	public static void main(String[] args) {
-		// 작업 진행도 배열 progresses
-		// 작업속도 배열 speeds
-		// Math.ceil((100 - prog)/speeds) 하면 작업완료 시점 나옴.
-		// 5 4 1 6 4 1 10
-		// 정답 List , 비교대상 tmp
+		// 테스트용 데이터
 		int[] progresses = {93, 30, 55};
 		int[] speeds = {1, 30, 5};
+
 		
+		// Math.ceil((100 - prog)/speeds) 으로 각 작업의 완료 시점을 구해준다.
 		int n = progresses.length;
 		int[] ends = new int[n];
-		
 		for (int i = 0; i < n; i++) {
 			ends[i] = (int) Math.ceil(1.0 * (100 - progresses[i]) / speeds[i]);
 		}
 		
+		// 작업 완료 시점을 비교할 변수, 결과를 저장할 List 생성
 		List<Integer> result = new ArrayList<>();
 		int tmp = ends[0];
-		
 		int cnt = 0;
 		
+		/**
+		 *  현재 가지고 있는 작업 완료 시점보다 큰 값을 발견하기 전까지는 모두 한 번에 배포할 수 있다.
+		 *  cnt 를 늘려나가다가, 큰 값을 발견하면 List 에 저장
+		 */
 		for (int i = 0; i < n; i++) {
 			if (tmp < ends[i]) {
 				result.add(cnt);
+				tmp = ends[i];
 				cnt = 1;
 			} else {
 				cnt++;
 			}
 		}
 		
+		// 마지막 작업에 대한 cnt 추가
 		result.add(cnt);
 		
-		
+		// return 할 배열 생성, List 의 타입은 Integer 라 toArray() 사용 불가
 		int[] answer =  new int[result.size()];
 		for (int i = 0; i < result.size(); i++) {
 			answer[i] = result.get(i);
 		}
 		
+		// stream 을 이용해 int[] 로 바꾸는 방법
+		// int[] answer = result.stream().mapToInt(i -> i).toArray();
+		
 		System.out.println(Arrays.toString(answer));
 		System.out.println(Arrays.toString(progresses));
 		System.out.println(Arrays.toString(speeds));
 		System.out.println(Arrays.toString(ends));
-//		return answer;
+		// return answer;
 	}
 
 }

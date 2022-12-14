988139f2b99ebda32f1775bd91891ed27ab95dbf
baebug <ufr134@gmail.com>
2022-12-15 00:04:04 +0900

    pg init

---
+++ java/algo/src/Solution.java
@@ -0,0 +1,11 @@
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {3, 1, 1, 1}));
+    }
+
+    static int solution(int[] nums) {
+
+        return 0;
+    }
+}
